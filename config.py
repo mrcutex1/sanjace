d1628a80f2907d700f656a2c1daecacb0f1bc2d1
ANURAG YADAV <139148328+mrcutex1@users.noreply.github.com>
2025-07-05 13:53:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,12 +18,12 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 300))
 
-# Vars For API End Pont.
-#YTPROXY_URL = getenv("YTPROXY_URL", 'http://tubed1.okflix.top/') ## E.G https://yt.okflix.
-#YT_API_KEY = "freekey15052025"
+Vars For API End Pont.
+YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## E.G https://yt.okflix.
+YT_API_KEY = getenv("YT_API_KEY" , None ) ##
 
-API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", 'NxGBNexGenBotse8e52d') # 
+#API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+#API_KEY = getenv("API_KEY", 'NxGBNexGenBotse8e52d') # 
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", -1001964294961))
