aa61ba6361d285f1903354ca3422f1715db48175
ANURAG YADAV <139148328+mrcutex1@users.noreply.github.com>
2025-07-05 14:16:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 300))
 
-Vars For API End Pont.
+#Vars For API End Pont.
 YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## E.G https://yt.okflix.
 YT_API_KEY = getenv("YT_API_KEY" , None ) ##
 
